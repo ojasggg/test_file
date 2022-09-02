a2c59aa3afd8c001a205c6af69cd21c2a10de650
Ojash Gurung <OJASGGG@GMAIL.COM>
2022-09-02 19:02:07 +0545

    This is my first commit.

---
+++ first.js
@@ -0,0 +1,59 @@
+// if (true) {
+//   console.log("print true");
+// } else {
+//   console.log("print false");
+// }
+
+true ? console.log("print true") : console.log("print false");
+
+// let name1 = "Ojash";
+// let name2 = "Gurung";
+// console.log(`print ${name1} ${name2}`);
+// console.log("print" + " " + name1 + " " + name2);
+
+// let num1, num2;
+// num1 = 10;
+// num2 = 20;
+
+// console.log(`Sum is : ${num1 + num2}`);
+
+// num1 > num2
+//   ? console.log(`1st ${num1} is greater`)
+//   : console.log(`${num2} is greater`);
+
+// const sum = function (a, b) {
+//   return a + b;
+// };
+
+// true ? console.log(true) : console.log(false);
+
+// const sum = (a, b) => {
+//   return a + b;
+// };
+
+// console.log(`Sum is ${sum(10, 20)}`);
+
+// switch (){
+//     case {
+//         //code
+//         // break
+//     }
+// }
+
+let fruits = ["apple", "mango", "orange"];
+
+for (fruits in arr) {
+  console.log(fruits);
+}
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (number of numbers) {
+  console.log(number);
+}
+
+// let int = 23;
+// let float = parseFloat(int);
+// console.log(float);
+
+// remote and local
